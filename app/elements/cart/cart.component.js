73c4b6828efb0d3f7e05f84221a284d468d50ba3
Artem Grachov <grachov2195@gmail.com>
2017-10-09 15:57:49 +0300

    Removing deleted products from cart

--- app/elements/cart/cart.component.js
+++ app/elements/cart/cart.component.js
@@ -1,10 +1,17 @@
 export default {
     controller: function (cartService, productsService) {
         this.cart = [];
+        this.total = 0;
         cartService.getCart()
-            .map(id => productsService
+            .map((id, index) => productsService
                 .getProduct(id)
-                .then(res => this.cart.push(res.name))
+                .then(res => {
+                    this.cart.push(res.name);
+                    this.total += res.price;
+                })
+                .catch(
+                    err => cartService.removeFromCart(index)
+                )
             )
         this.removeFromCart = function (index) {
             cartService.removeFromCart(index);
